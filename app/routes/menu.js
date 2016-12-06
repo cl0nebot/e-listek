74add8af4c77d425e3ca20d71af7c089e7d4fc2c
Petr Pikaus <pikaus.petr@gmail.com>
2016-12-06 23:35:24 +0100

    Fixed identation

--- app/routes/menu.js
+++ app/routes/menu.js
@@ -3,31 +3,31 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return [
-        {
-          title:'Nealkoholické nápoje',
-          category: 'non-alcoholic-drinks',
-          image: '/assets/images/nonalcoholic.png'
-        },
-        {
-          title:'Pivo',
-          category: 'beer-drinks',
-          image: '/assets/images/beer.png'
-        },
-        {
-          title:'Víno',
-          category: 'vine-drinks',
-          image: '/assets/images/wine.png'
-        },
-        {
-          title:'Teplé nápoje',
-          category: 'hot-drinks',
-          image: '/assets/images/hotdrinks.png'
-        },      {
-          title:'Destiláty',
-          category: 'alcoholic-drinks',
-          image: '/assets/images/distillates.png'
-        }
-      ];
+      {
+        title: 'Nealkoholické nápoje',
+        category: 'non-alcoholic-drinks',
+        image: '/assets/images/nonalcoholic.png',
+      },
+      {
+        title: 'Pivo',
+        category: 'beer-drinks',
+        image: '/assets/images/beer.png',
+      },
+      {
+        title: 'Víno',
+        category: 'vine-drinks',
+        image: '/assets/images/wine.png',
+      },
+      {
+        title: 'Teplé nápoje',
+        category: 'hot-drinks',
+        image: '/assets/images/hotdrinks.png',
+      }, {
+        title: 'Destiláty',
+        category: 'alcoholic-drinks',
+        image: '/assets/images/distillates.png',
+      },
+    ];
   },
 
   actions: {
