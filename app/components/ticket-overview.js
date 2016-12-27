cbf0cf174d762043759aebe0e268692276b6a4a4
Martin Morávek (moravek.martin) <moravek.martin@gmail.com>
2016-12-27 16:54:13 +0100

    colors

--- app/components/ticket-overview.js
+++ app/components/ticket-overview.js
@@ -3,38 +3,42 @@ const {inject: {service}, computed} = Ember;
 
 export default Ember.Component.extend({
   store: service(),
-  colors: service(),
+  isShowingModal: false,
+  _editedTicket: null,
+  name: null,
+
   tickets: computed(function() {
     return this.get('store').peekAll('ticket');
   }),
-  canUpdateTicket: computed('name', function() {
+
+  canUpdateTicket: computed('name', 'tickets.[]', function() {
     const name = this.get('name');
 
     if (!name) {
       return false;
     }
-    const ticket = this.get('store').peekAll('ticket').findBy('name', name);
+    const ticket = this.get('tickets').findBy('name', name);
     return !ticket || ticket === this.get('_editedTicket');
   }),
-  isShowingModal: false,
-  _editedTicket: null,
-  name: null,
+
   actions: {
     delete(ticket) {
-      this.get('colors').values.push(ticket.get('color'));
       ticket.destroyRecord();
     },
+
     updateTicket(ticket) {
       this.set('_editedTicket', ticket);
       this.set('name', ticket.get('name'));
       this.toggleProperty('isShowingModal');
     },
+
     submitUpdate(name) {
       const ticket = this.get('_editedTicket');
       ticket.set('name', name);
       ticket.save();
       this.toggleProperty('isShowingModal');
     },
+
     toggleModal() {
       this.toggleProperty('isShowingModal');
     },
