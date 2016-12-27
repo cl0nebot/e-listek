e1a160dcfa18f2c0ede3465a24987e98794b9710
Petr Pikaus <pikaus.petr@gmail.com>
2016-12-27 02:13:48 +0100

    Color reloading

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,11 +2,13 @@ import Ember from 'ember';
 import fixtures from '../fixtures/application';
 import parse from '../utils/fixtures-parser';
 const {RSVP} = Ember;
+const {inject: {service}} = Ember;
 
 export default Ember.Route.extend({
+  colors: service(),
   beforeModel() {
     const store = this.get('store');
-
+    this.get('colors').values;
     return RSVP.all([
       store.importData(parse(fixtures), {json: false}),
       store.findAll('ticket'),
--- app/services/colors.js
+++ app/services/colors.js
@@ -1,5 +1,20 @@
 import Ember from 'ember';
+const {inject: {service}} = Ember;
 
 export default Ember.Service.extend({
-  values: ['orange','blue','yellow','pink','olive','violet','green','purple','teal']
+  values: [],
+  store: service(),
+  init() {
+    this._super(...arguments);
+    this.reset();
+  },
+  reset(){
+    console.log('xx');
+    const values = ['orange','blue','yellow','pink','olive','violet','green','purple','teal'];
+    const self = this;
+    this.get('store').findAll('ticket', { reload: true }).then(function(tickets) {
+      const colors = tickets.getEach("color"); // ['first', 'second']
+      self.set('values',values.filter(c => !colors.includes(c)));
+    });
+  }
 });
